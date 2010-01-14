cd03cdc3effb60261240a23f54dc4e4b3eba09f4
Fred Wenzel <fwenzel@mozilla.com>
2010-01-14 19:19:17 +0100

    use gearman decorator to register "reverse" job

--- gearman_example/gearman.py
+++ gearman_example/gearman.py
@@ -2,7 +2,9 @@
 Example Gearman Job File.
 Needs to be called gearman.py and reside inside a registered Django app.
 """
+from django_gearman.decorators import gearman_job
 
+@gearman_job
 def reverse(input):
     """Reverse a string"""
     return input[::-1]
