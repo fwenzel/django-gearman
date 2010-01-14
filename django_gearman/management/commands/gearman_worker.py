a3b1b35c0782740b99862443d4cdefe2193e3f70
Fred Wenzel <fwenzel@mozilla.com>
2010-01-14 19:19:59 +0100

    management command to start a gearman worker with all registered jobs

---
+++ django_gearman/management/commands/gearman_worker.py
@@ -0,0 +1,44 @@
+from django.conf import settings
+from django.core.management.base import NoArgsCommand
+from django_gearman import GearmanWorker
+
+
+class Command(NoArgsCommand):
+    help = "Start a Gearman worker serving all registered German jobs"
+    __doc__ = help
+
+    def handle_noargs(self, **options):
+        # find gearman modules
+        gm_modules = []
+        for app in settings.INSTALLED_APPS:
+            try:
+                gm_modules.append(__import__("%s.gearman" % app))
+            except ImportError:
+                pass
+        if not gm_modules:
+            print "No gearman modules found!"
+            return
+
+        # instantiate a new worker and register all jobs
+        worker = GearmanWorker()
+        for gm_module in gm_modules:
+            try:
+                gm_module.gearman_jobs
+            except NameError:
+                continue
+            for job in gm_module.gearman_jobs:
+                # determine right name to register function with
+                app = job.app
+                jobname = job.__name__
+                try:
+                    func = settings.GEARMAN_JOB_NAME % {'app': app,
+                                                        'job': jobname}
+                except NameError:
+                    func = '%s.%s' % (app, jobname)
+                print "Registering job '%s'" % func
+                worker.register_function(func, job)
+
+        # start working
+        print "Starting to work..."
+        worker.work()
+
