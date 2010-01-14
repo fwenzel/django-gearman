d55e5123e07457ee46d3e84777f56cba55c85584
Fred Wenzel <fwenzel@mozilla.com>
2010-01-14 19:03:27 +0100

    decorator for registering gearman jobs

---
+++ django_gearman/decorators.py
@@ -0,0 +1,25 @@
+class gearman_job(object):
+    """
+    Decorator marking a function inside some_app/gearman.py as a gearman job
+    """
+
+    def __init__(self, f):
+        self.f = f
+        
+        # determine app name
+        parts = f.__module__.rpartition('.')
+        if parts[2]:
+            self.app = parts[0]
+        else:
+            self.app = ''
+
+        # store function in per-app job list (to be picked up by a worker)
+        gm_module = __import__(f.__module__)
+        try:
+            gm_module.gearman_jobs.append(self)
+        except AttributeError:
+            gm_module.gearman_jobs = [self]
+
+    def __call__(self, *args, **kwargs):
+        return self.f(*args, **kwargs)
+
