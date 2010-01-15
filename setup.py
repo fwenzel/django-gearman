bfde44eab024f21b9eafb49b1d997c831045bd87
Jeff Balogh <me@jeffbalogh.org>
2010-01-14 20:01:02 -0800

    get yourself a setup.py so we can install with pip

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-gearman',
+    version='0.0.1',
+    description='A convenience wrapper for Gearman clients and workers '
+                'in Django/Python',
+    long_description=open('README.md').read(),
+    author='Frederic Wenzel',
+    author_email='fwenzel@mozilla.com',
+    url='http://github.com/fwenzel/django-gearman',
+    license='MPL',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['gearman==1.5.0'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
